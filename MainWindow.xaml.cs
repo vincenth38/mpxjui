c9a4804ffbe0e68d5760fc24ca4998897b6c031d
vincenth38 <59369222+vincenth38@users.noreply.github.com>
2021-06-17 10:17:51 -0700

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -76,7 +76,8 @@ namespace MPXJUI
               // projectFile.getCustomFields().setAlias(TaskField.TEXT10, "task_code");
                
                //  projectFile.setTaskFieldAlias(TaskField.TEXT10, "task_code");
-                  projectFile.setEnterpriseCustomField(10,"task_code");
+             //     projectFile.setEnterpriseCustomField(10,"task_code");
+             projectFile.getCustomFields().getCustomField(11).setAlias("task_code");
                 MSPDIWriter writer = new MSPDIWriter();
                 foreach (Task task in projectFile.Tasks)
                 {
