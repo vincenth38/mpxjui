7612fe71bdfeb0e8aef7f7029b3b32d48c6c2b45
vincenth38 <59369222+vincenth38@users.noreply.github.com>
2021-07-04 12:03:43 -0700

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -73,7 +73,7 @@ namespace MPXJUI
                  ProjectFile projectFile = reader.read(file);
  
       
-                      Map customFields = reader.CustomFields();
+                      Map customFields = reader.CustomFieldMap();
            //   customFields.CustomField(TaskField.TEXT10).setAlias("task_code");
               
   
